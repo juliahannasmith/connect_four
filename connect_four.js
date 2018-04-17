e11acfd64596040214d90938f42fb6a3ec45df2f
Katherine R Linnenkohl <kl88081@srcs.loc>
2018-04-17 11:57:17 -0700

    red_button

--- connect_four.js
+++ connect_four.js
@@ -4,3 +4,11 @@ var table = [[0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0]]
+var button = document.getElementByClassName(button);
+
+function changeColor{
+    button.style.background = "red";
+
+
+}
+button.addEventListener("click", changeColor);
