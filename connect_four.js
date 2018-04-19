bc12f9b70f0fed8e7a6b30a71d156e97b769ebcb
Julia Smith <juliasmith@Julias-Air.hsd1.ca.comcast.net>
2018-04-18 21:07:33 -0700

    button vars on js

--- connect_four.js
+++ connect_four.js
@@ -1,15 +1,49 @@
+var button1x1 = document.getElementById('button1x1');
+var button1x2 = document.getElementById('button1x2');
+var button1x3 = document.getElementById('button1x3');
+var button1x4 = document.getElementById('button1x4');
+var button1x5 = document.getElementById('button1x5');
+var button1x6 = document.getElementById('button1x6');
+var button1x7 = document.getElementById('button1x7');
+var button2x1 = document.getElementById('button2x1');
+var button2x2 = document.getElementById('button2x2');
+var button2x3 = document.getElementById('button2x3');
+var button2x4 = document.getElementById('button2x4');
+var button2x5 = document.getElementById('button2x5');
+var button2x6 = document.getElementById('button2x6');
+var button2x7 = document.getElementById('button2x7');
+var button3x1 = document.getElementById('button3x1');
+var button3x2 = document.getElementById('button3x2');
+var button3x3 = document.getElementById('button3x3');
+var button3x4 = document.getElementById('button3x4');
+var button3x5 = document.getElementById('button3x5');
+var button3x6 = document.getElementById('button3x6');
+var button3x7 = document.getElementById('button3x7');
+var button4x1 = document.getElementById('button4x1');
+var button4x2 = document.getElementById('button4x2');
+var button4x3 = document.getElementById('button4x3');
+var button4x4 = document.getElementById('button4x4');
+var button4x5 = document.getElementById('button4x5');
+var button4x6 = document.getElementById('button4x6');
+var button4x7 = document.getElementById('button4x7');
+var button5x1 = document.getElementById('button5x1');
+var button5x2 = document.getElementById('button5x2');
+var button5x3 = document.getElementById('button5x3');
+var button5x4 = document.getElementById('button5x4');
+var button5x5 = document.getElementById('button5x5');
+var button5x6 = document.getElementById('button5x6');
+var button5x7 = document.getElementById('button5x7');
+var button6x1 = document.getElementById('button6x1');
+var button6x2 = document.getElementById('button6x2');
+var button6x3 = document.getElementById('button6x3');
+var button6x4 = document.getElementById('button6x4');
+var button6x5 = document.getElementById('button6x5');
+var button6x6 = document.getElementById('button6x6');
+var button6x7 = document.getElementById('button6x7');
 
-var table = [[0, 0, 0 ,0 ,0, 0, 0],
-             [0, 0, 0 ,0 ,0, 0, 0],
-             [0, 0, 0 ,0 ,0, 0, 0],
-             [0, 0, 0 ,0 ,0, 0, 0],
-             [0, 0, 0 ,0 ,0, 0, 0],
-             [0, 0, 0 ,0 ,0, 0, 0]]
-
-var button = document.getElementByClassName(button);
-
-function changeColor{
-    button.style.background = "red";
+function changeColor() {
+    button1x1.style.background = '#ff0000';
 
 }
-button.addEventListener("click", changeColor);
+
+button1x1.addEventListener('click', changeColor);
