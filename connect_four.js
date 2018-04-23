6bcce1eaa997291d281bdca9fb3d6fb0c9cb5ed9
Julia H Smith <js44684@srcs.loc>
2018-04-23 11:17:50 -0700

    added css to table

--- connect_four.js
+++ connect_four.js
@@ -10,7 +10,6 @@ function changeColor(buttonID) {
     buttonID.style.background = '#ff0000';
 }
 
-
 var tablearea = document.getElementById('tablearea');
 var table = document.createElement('table');
 tablearea.appendChild(table);
@@ -20,7 +19,11 @@ var button = document.createElement('button');
 for (var i = 1; i < 7; i++) {
     tr[i] = document.createElement('tr');
     for (var j = 1; j < 8; j++) {
-        td1 = td1.appendChild(document.createElement('button'));
+        button = document.createElement('button');
+        td1 = td1.appendChild(button);
+        button.setAttribute('class', 'button');
+        button.setAttribute('id', 'button' + i + 'x' + j)
+        button.addEventListener("click", changeColor)
         tr[i].appendChild(td1);
     }
     table.appendChild(tr[i]);
