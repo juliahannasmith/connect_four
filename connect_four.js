58155324abc8a8552de5deacc908c31d3cf5e0fe
Julia Smith <juliasmith@Julias-Air.hsd1.ca.comcast.net>
2018-04-22 22:25:02 -0700

    fixed code for table

--- connect_four.js
+++ connect_four.js
@@ -1,4 +1,4 @@
-var table = [[0, 0, 0, 0, 0, 0, 0],
+var tableVar = [[0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0],
@@ -11,40 +11,16 @@ function changeColor(buttonID) {
 }
 
 
-
-
 var tablearea = document.getElementById('tablearea');
-
 var table = document.createElement('table');
-
-var tr = [];
-
-var td1 = document.createElement('td');
-var td2 = document.createElement('td');
-
-var button = document.createElement('button');
-
-for (var i = 1; i < 7; i++) {
-    tr[i] = document.createElement('tr');
-    for (var j = 1; j < 8; j++) {
-        tr[i].appendChild(td1);
-        td1.appendChild(button);
-
-    }
-    table.appendChild(tr[i]);
-
-}
-
-
-
-var tablearea = document.getElementById('tablearea');
 tablearea.appendChild(table);
-
+var tr = [];
+var td1 = document.createElement('td');
+var button = document.createElement('button');
 for (var i = 1; i < 7; i++) {
     tr[i] = document.createElement('tr');
     for (var j = 1; j < 8; j++) {
-        var td1 = document.createElement('td');
-        td1.appendChild(document.createElement('Button'));
+        td1 = td1.appendChild(document.createElement('button'));
         tr[i].appendChild(td1);
     }
     table.appendChild(tr[i]);
