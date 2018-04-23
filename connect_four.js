475f197495b2557f848518ed984cdaf52520c237
Julia Smith <juliasmith@Julias-Air.hsd1.ca.comcast.net>
2018-04-22 20:38:10 -0700

    SOME SORT OF TABLE!!! (extra button)

--- connect_four.js
+++ connect_four.js
@@ -12,6 +12,7 @@ function changeColor(buttonID) {
 
 
 
+
 var tablearea = document.getElementById('tablearea');
 
 var table = document.createElement('table');
@@ -23,13 +24,28 @@ var td2 = document.createElement('td');
 
 var button = document.createElement('button');
 
-for (var i = 1; i < 6; i++) {
+for (var i = 1; i < 7; i++) {
     tr[i] = document.createElement('tr');
-    for (var j = 1; j < 7; j++) {
-        tr[i].appendChild(button);
+    for (var j = 1; j < 8; j++) {
+        tr[i].appendChild(td1);
+        td1.appendChild(button);
+
     }
     table.appendChild(tr[i]);
 
 }
 
+
+
+var tablearea = document.getElementById('tablearea');
 tablearea.appendChild(table);
+
+for (var i = 1; i < 7; i++) {
+    tr[i] = document.createElement('tr');
+    for (var j = 1; j < 8; j++) {
+        var td1 = document.createElement('td');
+        td1.appendChild(document.createElement('Button'));
+        tr[i].appendChild(td1);
+    }
+    table.appendChild(tr[i]);
+}
