472545204675ad3cf7537a8572bd5fc6885580f0
Julia H Smith <js44684@srcs.loc>
2018-04-23 12:20:38 -0700

    css table

--- connect_four.js
+++ connect_four.js
@@ -15,15 +15,14 @@ var table = document.createElement('table');
 tablearea.appendChild(table);
 var tr = [];
 var td1 = document.createElement('td');
-var button = document.createElement('button');
 for (var i = 1; i < 7; i++) {
     tr[i] = document.createElement('tr');
     for (var j = 1; j < 8; j++) {
-        button = document.createElement('button');
+        var button = document.createElement('button');
         td1 = td1.appendChild(button);
         button.setAttribute('class', 'button');
-        button.setAttribute('id', 'button' + i + 'x' + j)
-        button.addEventListener("click", changeColor)
+        button.setAttribute('id', 'button' + i + 'x' + j);
+        button.addEventListener('click', changeColor);
         tr[i].appendChild(td1);
     }
     table.appendChild(tr[i]);
